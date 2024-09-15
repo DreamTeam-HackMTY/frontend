ce04414bc3f055386bc15a7d4a5b187c68c392fe
Angel Vargas <angelvrg130@gmail.com>
2024-09-14 19:31:32 -0600

    add routes conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3498db',
+        secondary: '#2c3e50',
+    }
+    },
   },
   plugins: [],
 }
