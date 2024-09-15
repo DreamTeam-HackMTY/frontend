826174cd4e14b11735037d9b0d1ea48d20e62575
Angel Vargas <angelvrg130@gmail.com>
2024-09-15 00:03:55 -0600

    add gurds

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { VerifySession, VerifyLogin } from './Guards/AuthGuard';
 import login from './Pages/Login.vue';
 import Home from './Pages/Home.vue'
 import Enfermedades from './Pages/Enfermedades.vue'
 
 const routes = [
-  { path: '/login', component: login },
-  { 'path': '/', component: Home },
+  { path: '/login', component: login, beforeEnter: VerifyLogin },
+  { 'path': '/', component: Home, beforeEnter: VerifySession },
   { 'path': '/enfermedades', component: Enfermedades }
 ];
 
